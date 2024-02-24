62473a54642fb53a1ea339db267c631f8a4836c6
Andres Yebra <andres.yebracervantes@gmail.com>
2024-02-23 20:44:14 -0800

    feat: Task table

--- db/migrate/20240224043747_add_task_table.rb
+++ db/migrate/20240224043747_add_task_table.rb
@@ -2,8 +2,6 @@ class AddTaskTable < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
       t.string :name, null: false
-      t.belongs_to :user
-
       t.timestamps
     end
   end
